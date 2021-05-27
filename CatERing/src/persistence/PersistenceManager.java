d977bc1bb64cdad61be68d71c67a499f030c84a1
fedeztk <federicoserranexus@gmail.com>
2021-05-27 23:07:21 +0200

    fix ssd merge generalizzazione turni

--- CatERing/src/persistence/PersistenceManager.java
+++ CatERing/src/persistence/PersistenceManager.java
@@ -5,9 +5,9 @@ import com.sun.javafx.binding.StringFormatter;
 import java.sql.*;
 
 public class PersistenceManager {
-    private static String url = "jdbc:mysql://localhost:8889/catering?serverTimezone=UTC";
+    private static String url = "jdbc:mysql://localhost:3306/catering?serverTimezone=UTC";
     private static String username = "root";
-    private static String password = "root";
+    private static String password = "";
 
     private static int lastId;
 
